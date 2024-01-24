3da4a4690fc0417e282dfb4ac4eafed895cd598b
John Rocha <johnerickrocha@gmail.com>
2024-01-24 01:52:27 -0300

    start(up repo): initial

---
+++ pages/about.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="text-5xl font-extrabold ...">
+    <span
+      class="bg-clip-text text-transparent bg-gradient-to-r from-pink-500 to-violet-500"
+    >
+      Hello world
+    </span>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped></style>
---
+++ pages/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <div class="text-5xl font-extrabold">
+  <span class="bg-clip-text text-transparent bg-gradient-to-r from-pink-500 to-violet-500">
+    New new 💖
+  </span>
+</div>
+  </div>
+</template>
+
+<script>
+export default {
+  setup() {
+    return {};
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
